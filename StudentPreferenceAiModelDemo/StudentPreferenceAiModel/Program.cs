38f289d9121aac59ac7f3cf475c4bf44b6822619
tumelo <tumelom@evolvemed.co.za>
2020-08-09 20:25:55 +0200

    added projet files, gitignore and git attributes

---
+++ StudentPreferenceAiModelDemo/StudentPreferenceAiModel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentPreferenceAiModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
