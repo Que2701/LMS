adcd47df531a03aadac42a605064684382c3ec95
tumelo <tumelom@evolvemed.co.za>
2020-08-10 09:52:15 +0200

    added training and testing files

---
+++ StudentPreferenceAiModelDemo/StudentPreferenceAiModel/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace StudentPreferenceAiModel.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LearningPreference { get; set; }
+    }
+}
--- StudentPreferenceAiModelDemo/StudentPreferenceAiModel/Program.cs
+++ StudentPreferenceAiModelDemo/StudentPreferenceAiModel/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using StudentPreferenceAiModel.Models;
 
 namespace StudentPreferenceAiModel
 {
@@ -6,7 +8,36 @@ namespace StudentPreferenceAiModel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Student Preference Ai Model";
+
+            // Add input data
+            // var input = new ModelInput();
+
+            Console.WriteLine("Press 'Q' to close program");
+            do
+            {
+                Console.WriteLine("Enter first name: ");
+                var firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter last name: ");
+                var lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter your student number: ");
+                var studentNumber = Console.ReadLine();
+
+                Console.WriteLine("Describe your study preferences: ");
+                // input.Description = Console.ReadLine();
+
+                // Load model and predict output of sample data
+                // ModelOutput result = ConsumeModel.Predict(input);
+
+                Console.WriteLine($"The recommended study preference for {firstName} {lastName} student number {studentNumber}:");
+                // Console.WriteLine($"Preference: {result.Prediction}\tAccurary: {result.Score[0]}");
+            } while (Console.ReadKey().Key != ConsoleKey.Q);
+
+
+            // Allow the user to close the application
+            // Console.ReadLine();
         }
     }
 }
